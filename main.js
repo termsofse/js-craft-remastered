85f78c671585e4784a39876426fabe38c622b8c6
Termsofse <143139189+termsofse@users.noreply.github.com>
2026-04-07 17:51:26 -0500

    remove incompatible meshes

--- main.js
+++ main.js
@@ -14,50 +14,35 @@ scene.add( sunlight );
 const boxGeo = new THREE.BoxGeometry(1, 1, 1);
 const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const basicMesh = new THREE.Mesh(boxGeo, basicMaterial);
-basicMesh.position.x = 5;
+basicMesh.position.x = 6;
 scene.add(basicMesh);
-
-const depthMaterial = new THREE.MeshDepthMaterial({ color: 0x00ff00 });
-const depthMesh = new THREE.Mesh(boxGeo, depthMaterial);
-depthMesh.position.x = 4;
-scene.add(depthMesh);
-
-const distanceMaterial = new THREE.MeshDistanceMaterial({ color: 0x00ff00 });
-const distanceMesh = new THREE.Mesh(boxGeo, distanceMaterial);
-distanceMesh.position.x = 3;
-scene.add(distanceMesh);
 
 const lambertMaterial = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 const lambertMesh = new THREE.Mesh(boxGeo, lambertMaterial);
-lambertMesh.position.x = 2;
+lambertMesh.position.x = 5;
 scene.add(lambertMesh);
 
 const matcapMaterial = new THREE.MeshMatcapMaterial({ color: 0x00ff00 });
 const matcapMesh = new THREE.Mesh(boxGeo, matcapMaterial);
-matcapMesh.position.x = 1;
+matcapMesh.position.x = 4;
 scene.add(matcapMesh);
-
-const normalMaterial = new THREE.MeshNormalMaterial({ color: 0x00ff00 });
-const normalMesh = new THREE.Mesh(boxGeo, normalMaterial);
-normalMesh.position.x = 0;
-scene.add(normalMesh);
 
 const phongMaterial = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
 const phongMesh = new THREE.Mesh(boxGeo, phongMaterial);
-phongMesh.position.x = 5;
+phongMesh.position.x = 3;
 scene.add(phongMesh);
 
 const physicalMaterial = new THREE.MeshPhysicalMaterial({ color: 0x00ff00 });
 const physicalMesh = new THREE.Mesh(boxGeo, physicalMaterial);
-physicalMesh.position.x = -1;
+physicalMesh.position.x = 2;
 scene.add(physicalMesh);
 
 const standardMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const standardMesh = new THREE.Mesh(boxGeo, standardMaterial);
-standardMesh.position.x = -2;
+standardMesh.position.x = 1;
 scene.add(standardMesh);
 
 const toonMaterial = new THREE.MeshToonMaterial({ color: 0x00ff00 });
 const toonMesh = new THREE.Mesh(boxGeo, toonMaterial);
-toonMesh.position.x = -3;
+toonMesh.position.x = 0;
 scene.add(toonMesh);
