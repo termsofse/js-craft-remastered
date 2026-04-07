cc1fcf2d8b75d3cb9168b914f8fdc81fc133bfb1
Termsofse <143139189+termsofse@users.noreply.github.com>
2026-04-07 18:05:17 -0500

    make small changes

--- main.js
+++ main.js
@@ -8,8 +8,21 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 // #endregion
 
+const sunlight = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add(sunlight);
 const boxGeo = new THREE.BoxGeometry(1, 1, 1);
-const mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const mat = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const mesh = new THREE.Mesh(boxGeo, mat);
 scene.add(mesh);
-renderer.render(scene, camera);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate the cube for some life
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();
